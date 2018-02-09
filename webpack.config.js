956a224c7bf827809ab4cbad5e90a7434ae8be48
Kevin J Donohue <kevinjdonohue@yahoo.com>
2018-02-08 22:10:31 -0800

    Adding configuration to webpack to use style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,14 @@ module.exports = {
   entry: ['webpack-dev-server/client?http://localhost:8080', 'webpack/hot/only-dev-server', './src/index.jsx'],
   module: {
     rules: [
-      // {
-      //   enforce: 'pre',
-      //   test: /\.(js|jsx)$/,
-      //   exclude: /node_modules/,
-      //   loader: 'eslint-loader',
-      // },
       {
         test: /\.(js|jsx)?$/,
         exclude: /node_modules/,
         loader: 'react-hot-loader/webpack!babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
       },
     ],
   },
