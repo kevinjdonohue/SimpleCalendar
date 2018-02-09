685ef8cff216081bba4ad7d7baf2b0659e710ee0
Kevin J Donohue <kevinjdonohue@yahoo.com>
2018-02-08 23:13:00 -0800

    adding some divs and CSS styling in order to play around with the layout using grid

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
 
-const Calendar = () => <div>This is the calendar component</div>;
+import './Calendar.css';
+
+const Calendar = () => (
+  <div className="calendar">
+    <div className="calendar__header--odd">Sunday</div>
+    <div className="calendar__header">Monday</div>
+    <div className="calendar__header--odd">Tuesday</div>
+    <div className="calendar__header">Wednesday</div>
+    <div className="calendar__header--odd">Thursday</div>
+    <div className="calendar__header">Friday</div>
+    <div className="calendar__header--odd">Saturday</div>
+
+    <div className="calendar__header--odd">1</div>
+    <div className="calendar__header">2</div>
+    <div className="calendar__header--odd">3</div>
+    <div className="calendar__header">4</div>
+    <div className="calendar__header--odd">5</div>
+    <div className="calendar__header">6</div>
+    <div className="calendar__header--odd">7</div>
+  </div>
+);
 
 export default Calendar;
