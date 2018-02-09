5d7feef343d5cf4517728b8d11891bbe6289fa69
Kevin J Donohue <kevinjdonohue@yahoo.com>
2018-02-08 22:11:21 -0800

    adding a stylesheet for basic app formatting

--- src/SimpleCalendarApp.jsx
+++ src/SimpleCalendarApp.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Calendar from './Calendar';
 
+import './SimpleCalendarApp.css';
+
 const SimpleCalendarApp = () => (
   <div className="simple-calendar-app">
     <h1>Simple Calendar</h1>
